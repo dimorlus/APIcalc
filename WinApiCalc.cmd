9a9419f966b7e6279c1b511806ad60251c11bf38
Dmitry Orlov <dimorlus@gmail.com>
2025-11-06 22:05:46 +0200

    06-Nov-25 22:05:46.07

--- WinApiCalc.cmd
+++ WinApiCalc.cmd
@@ -1,5 +1,6 @@
 "c:\Espressif\utils\vcntr.exe" -csubver.cfg all -d -q
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" WinApiCalc.vcxproj /p:Configuration=Release /p:Platform=x64
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" WinApiCalc.vcxproj /p:Configuration=Release /p:Platform=x64 /t:Rebuild
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" WinApiCalc.vcxproj /p:Configuration=Release /p:Platform=x86 /t:Rebuild
 pause
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" fcalc.iss
 "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" fcalc5.iss

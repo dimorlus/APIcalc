8bece58673df1ca75e4ef07d94f876ecab77e81c
Dmitry Orlov <dimorlus@gmail.com>
2026-03-28 23:54:44 +0300

    28-Mar-26 23:54:40.95  v2.157L

--- stp.cmd
+++ stp.cmd
@@ -2,6 +2,9 @@
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" fcalc.iss
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" fcalc_w10+64.iss
 "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" fcalc5.iss
+cd BCB
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" fcalc.iss
+cd..
 echo "Run setup"
 pause
 .\Setup\fcalc_w10+64_std_dll_setup.exe
